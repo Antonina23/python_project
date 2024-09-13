3b5c14df9e16bdf68ca84b571050e1ecd24ee88a
Antonina <tone4kawow@gmail.com>
2024-09-13 18:33:40 +0400

    List

---
+++ pythonProject/my_projects/list.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry"]
+fruits.append("watermelon")
+
+print(fruits)
+print("cherry" in fruits)
+
+numbers = [100, 0, 1, 5, 78, 56, 9, 16]
+numbers.sort()
+print(numbers)
+
+
