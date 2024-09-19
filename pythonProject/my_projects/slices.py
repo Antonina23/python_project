1fec4ae3ad8b28bd41a857869fe23879fbd252f3
Antonina <tone4kawow@gmail.com>
2024-09-19 20:04:47 +0400

    List

---
+++ pythonProject/my_projects/slices.py
@@ -0,0 +1,9 @@
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+new_numbers = numbers[0:5]
+second_numbers = numbers[0:len(numbers)]
+third_numbers = numbers[0::2]
+
+print(new_numbers)
+print(second_numbers)
+print(third_numbers)
